a74e68316e992c613246984604f547bc0f300978
Jon Redeker <jon.redeker@gmail.com>
2022-10-17 23:32:54 -0400

    add tailwindcss with prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
